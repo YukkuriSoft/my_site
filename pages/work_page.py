c1deb79a86f22b5421bfc0c43e6e80081a4b06ee
YukkuriSoft <codejapan@outlook.jp>
2026-02-22 15:49:28 +0900

    11

--- pages/work_page.py
+++ pages/work_page.py
@@ -15,7 +15,7 @@ stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 bg_color = st.sidebar.color_picker("Background color hex: ", "#FFF")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 
-# realtime_update = st.sidebar.checkbox("Update in realtime", True)
+realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
 
 # Create a canvas component
@@ -25,18 +25,19 @@ canvas_result = st_canvas(
     stroke_color=stroke_color,
     background_color=bg_color,
     background_image=Image.open(bg_image) if bg_image else None,
-    # update_streamlit=realtime_update,
-    height=150,
+    update_streamlit=realtime_update,
+    width=300,
+    height=500,
     drawing_mode=drawing_mode,
     point_display_radius=point_display_radius if drawing_mode == "point" else 0,
     key="canvas",
 )
 
 # Do something interesting with the image data and paths
-if canvas_result.json_data is not None:
-    objects = pd.json_normalize(
-        canvas_result.json_data["objects"]
-    )  # need to convert obj to str because PyArrow
-    for col in objects.select_dtypes(include=["object"]).columns:
-        objects[col] = objects[col].astype("str")
-    st.dataframe(objects)
+# if canvas_result.json_data is not None:
+#     objects = pd.json_normalize(
+#         canvas_result.json_data["objects"]
+#     )  # need to convert obj to str because PyArrow
+#     for col in objects.select_dtypes(include=["object"]).columns:
+#         objects[col] = objects[col].astype("str")
+#     st.dataframe(objects)
